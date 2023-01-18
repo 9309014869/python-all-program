d70c7000c4fe89fbc9de240abd1e8f9c776b44e1
TEJAS GAJANAN KAWARKHE <112716745+9309014869@users.noreply.github.com>
2023-01-18 19:11:11 +0530

    Create singleinheristanse.py

---
+++ singleinheristanse.py
@@ -0,0 +1,20 @@
+
+print("Hello World")
+class Tejas:
+    def show(self):
+        print("iam class")
+        
+        
+class Abhishek(Tejas):
+    
+    def display(self):
+        r=range(1,10,2)
+        for i in r:
+            print(i)
+emp= Abhishek()
+emp.display()
+emp.show()
+            
+           
+      
+    
